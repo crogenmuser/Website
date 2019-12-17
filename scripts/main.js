58cf25d845826b53aff4f94e51ebfccdf18f88de
Matt Fossett <mattfoss01@gmail.com>
2019-12-16 21:51:52 -0500

    picture

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+
+function openNav() {
+document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+document.getElementById("mySidenav").style.width = "0";
+}

1ce925f158c481b0d1a406196a148b7e68fe5a48
Matt Fossett <mattfoss01@gmail.com>
2019-12-17 19:04:13 -0500

    picture

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,15 @@
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    //document.getElementById("hamburger").style.display = "none";
-    $("#hamburger").fadeOut();
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    //document.getElementById("hamburger").style.display = "block";
-    $("#hamburger").fadeIn();
-}
+$(document).ready(function () {
+    //your code here
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+        //document.getElementById("hamburger").style.display = "none";
+        $("#hamburger").fadeOut();
+    }
+    
+    /* Set the width of the side navigation to 0 */
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        //document.getElementById("hamburger").style.display = "block";
+        $("#hamburger").fadeIn();
+    }
+  });

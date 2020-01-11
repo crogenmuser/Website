c01c535d3419518abee78a1e6eb85c4d2491ddd8
Matt Fossett <mattfoss01@gmail.com>
2020-01-11 14:32:32 -0500

    Hello

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,19 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     $("#hamburger").fadeIn();
 }
+
+function parseInput(){
+    var fName = document.getElementById('formFName').value;
+    var lName = document.getElementById('formLName').value;
+    
+    var subject = document.getElementById('formSubject').value;
+    var body = document.getElementById('formMessage').value;
+
+    var text = 'Subject: ' + subject + '\n\nMessage: ' + body;    
+    document.getElementById('names').value = fName + ' ' + lName;
+
+    document.getElementById('goodMessages').value = text;
+
+    // submit
+    document.getElementById('actualSubmit').submit();
+}
